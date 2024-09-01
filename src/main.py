b2977b4d1161ce31a288bd3541e0d5c5f0a25727
jackma-00 <jacopo.maragna1@gmail.com>
2024-09-01 11:27:30 +0200

    cors middleware

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import event_request, event_task, staff_request, financial_request
 
 app = FastAPI()
 
+# Configure CORS
+origins = [
+    "*",  # Allow all origins for simplicity; adjust as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(event_request.router)
 app.include_router(event_task.router)
