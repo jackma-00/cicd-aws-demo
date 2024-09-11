a9b412d424afbc8d2654f278e2d57477bd9f3fd5
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-09-11 10:11:46 +0200

    Update main.py

--- assets/src/main.py
+++ assets/src/main.py
@@ -15,7 +15,7 @@ app.include_router(financial_request.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Spoon is an average JavaScript code analyzer"}
+    return {"message": "Spoon is an average Java code analyzer"}
 
 
 @app.get("/health")
